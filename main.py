1476ef6c176f93a3c16f3b2588203e971fc16b72
Isabel Solano <misolanob@gmail.com>
2023-02-02 23:06:22 -0600

    Attendance: fixed string to int

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ import pandas as pd
 
 # importar el conjunto de datos
 datos = pd.read_csv('baseball_reference_2016_scrape.csv')
+
+# arreglar columnas antes de guardar los datos
+
+
 X = datos.iloc[:, :-1].values
 y = datos.iloc[:, -1].values
 
@@ -32,14 +36,25 @@ print(datos.iloc[:, 0])
 
 # limpieza columna por columna -------
 
-# Attendance: remove "']"
-for x in range(len(datos.iloc[:, 0])):
-    string_temp = datos.iloc[:, 0][x][:-2]
+# Attendance: remove "']" at the end
+for x in range(len(X[:, 0])):
+    string_temp = X[:, 0][x][:-2]
     string_temp = string_temp.replace(",", "")
     int_temp = int(string_temp)
-    datos.iloc[:, 0][x] = int_temp
-    
+    X[:, 0][x] = int_temp
 
-for x in datos.iloc[:, 0]:
-    print(x)
 
+
+print("\nprint(X[:, 0])")
+print(X[:, 0])
+
+# Venue: remove ":" at the beginning
+
+# for x in range(len(X[:, 16])):
+#     string_temp = X[:, 16][x].replace(": ", "")
+#     X[:, 16][x] = string_temp
+
+print("\nprint(X[:, 16])")
+print(X[:, 13])
+
+# print(X)
