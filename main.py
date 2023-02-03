c7c71de40844394bfff3b61bd13aec532ec9374a
Isabel Solano <misolanob@gmail.com>
2023-02-02 22:28:14 -0600

    Attendance: fixed string to int

--- main.py
+++ main.py
@@ -3,6 +3,43 @@
 # Departamento de Ciencias de la Computación
 # Minería de Datos
 
-# Christopher García
-# Ma. Isabel Solano
+# Christopher García 20541
+# Ma. Isabel Solano 20504
+
+# impotar librarías
+
+import numpy as np
+import matplotlib as plt
+import pandas as pd
+
+
+# importar el conjunto de datos
+datos = pd.read_csv('baseball_reference_2016_scrape.csv')
+X = datos.iloc[:, :-1].values
+y = datos.iloc[:, -1].values
+
+print("\nDatos head")
+print(datos.head())
+
+print("\nX")
+# print(X)
+
+print("\ny")
+print(y)
+
+print("\ndatos.ilos[:, 0]")
+print(datos.iloc[:, 0])
+
+# limpieza columna por columna -------
+
+# Attendance: remove "']"
+for x in range(len(datos.iloc[:, 0])):
+    string_temp = datos.iloc[:, 0][x][:-2]
+    string_temp = string_temp.replace(",", "")
+    int_temp = int(string_temp)
+    datos.iloc[:, 0][x] = int_temp
+    
+
+for x in datos.iloc[:, 0]:
+    print(x)
 
